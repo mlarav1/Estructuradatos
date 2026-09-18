6b8ecee158ec735eda02e5f08bbc20b5157352b9
Miguel Lara <mlarav1@unicartagena.edu.co>
2026-09-18 11:38:16 -0500

    feat: reemplazar impares por cero

--- cpp/arreglos.cpp
+++ cpp/arreglos.cpp
@@ -33,5 +33,20 @@ cout << "Recorrido con for-each:" << endl;
 for (int numero : numeros) {
     cout << numero << endl;
 }
+
+cout << "Arreglo después de cambiar los impares por cero:" << endl;
+
+for (int i = 0; i < 10; i++) {
+    if (numeros[i] % 2 != 0) {
+        numeros[i] = 0;
+    }
+}
+
+for (int i = 0; i < 10; i++) {
+    cout << numeros[i] << " ";
+}
+
+cout << endl;
+         
          return 0;
 }
